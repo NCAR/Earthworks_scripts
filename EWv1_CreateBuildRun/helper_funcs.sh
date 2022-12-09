fc9e09a2956aa820272bcb1d3bd15568499789d9
G. Dylan Dickerson <gdicker@ucar.edu>
2022-12-09 16:31:55 -0700

    Add function to easily print arrays for EWv1_CreateBuildRun scripts

--- EWv1_CreateBuildRun/helper_funcs.sh
+++ EWv1_CreateBuildRun/helper_funcs.sh
@@ -16,6 +16,12 @@ function vexec() {
   [ $VERBOSITY -ge 2 ] && $1 || $1 > /dev/null 2>&1
 }
 
+function print_arr() {
+  # Override IFS to a comma and echo input array
+  local IFS=", "
+  echo "$*"
+}
+
 function usage() {
   # Print information about how to use this script
   # usage [{return val} {messages...}]
@@ -153,8 +159,8 @@ echo -e "Using:"
 echo -e "\tEarthWorks Repo at                   \"$SRCROOT\""
 echo -e "\tCreating cases in                    \"$CASES_DIR\""
 echo -e "\tCase bld and run directories will be \"${OUTPUTROOT}/\${CASENAME}\""
-echo -e "\tRunning with compiler                \"${C_SUITES[@]}\""
-echo -e "\tOn MPAS-A grids (km)                 \"${RESS[@]}\""
+echo -e "\tRunning with compilers               \"$(print_arr ${C_SUITES[@]})\""
+echo -e "\tOn MPAS-A grids (km)                 \"$(print_arr ${RESS[@]})\""
 if [ "$OVERWRITE" = true ]; then
   echo -e "\tCREATE=$DO_CREATE\tBUILD=$DO_BUILD\tRUN=$DO_RUN\tOVERWRITE=true"
 else
