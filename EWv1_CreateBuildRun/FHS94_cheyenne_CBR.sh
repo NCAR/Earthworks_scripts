8d08d4a83796e9d799f012904167d7fa5c3dcc82
G. Dylan Dickerson <gdicker@ucar.edu>
2023-03-03 11:45:50 -0700

    Fix casename generation error in FHS94

--- EWv1_CreateBuildRun/FHS94_cheyenne_CBR.sh
+++ EWv1_CreateBuildRun/FHS94_cheyenne_CBR.sh
@@ -64,7 +64,7 @@ for NTASKS in ${NTASKSS[@]:-"0"}; do
   #############################################################################
   # End Set loop vars
   #############################################################################
-  CASE=$(printf "%s%s.mpasa%03d.%s.%s" "${PRE:+${PRE}_}" "$C_SUITE_SUITE_SUITEOMP" "$RES" "$MACH" "$C_SUITE")
+  CASE=$(printf "%s%s.mpasa%03d.%s.%s" "${PRE:+${PRE}_}" "$COMP" "$RES" "$MACH" "$C_SUITE")
   [ $NTASKS -ne 0 ] && CASE="${CASE}.${NTASKS}"
   CASEROOT="${CASES_DIR}/${CASE}"
   GRID=$(printf "mpasa%03d_mpasa%03d" $RES $RES)
