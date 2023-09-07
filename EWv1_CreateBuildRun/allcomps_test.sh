cfd99e88c16fb7aa502d2ad86d102fb8c9c047bd
G. Dylan Dickerson <gdicker@ucar.edu>
2023-09-07 17:22:50 -0600

    Update default paths for easier use in allcomps_test.sh

--- EWv1_CreateBuildRun/allcomps_test.sh
+++ EWv1_CreateBuildRun/allcomps_test.sh
@@ -1,22 +1,27 @@
 #!/usr/bin/env bash
 
+# Intended to be run:
+# bash allcomps_test.sh 2>&1 | tee log.attXX.txt
+
 COMPS=("FHS94" "FKESSLER" "F2000climo" "QPC6" "FullyCoupled")
 # Edit these paths to whatever works for you
-SRCROOT="/glade/work/gdicker/EarthWorks/2023Jun22_Beta14Update/EarthWorks"
-CSEROOT="/glade/work/gdicker/EarthWorks/2023Jun22_Beta14Update/cases"
+# Assumes script is in "EarthWorks/tools/Earthworks_scripts/EWv1_CreateBuildRun"
+SRCROOT="$(realpath ${PWD}/../../../)" # Points to EarthWorks
+CSEROOT="$(realpath ${PWD}/../../../../cases)"
+[ -d "${CSEROOT}" ] || mkdir -p "${CSEROOT}"
 # Adjust case prefix to whatever makes sense for you
-PRE="$(date +%Y%b%d_%H%M%S)"
+PRE="$(date +%Y%b%d_%H%M%S)_alpha16dUpdate"
 
 #CMD="_cheyenne_CBR.sh --srcroot ${SRCROOT} --casesdir ${CSEROOT} --res=(120 60 30) --compiler=(gnu intel) -cp ${PRE} "
-CMD="_cheyenne_CBR.sh --srcroot ${SRCROOT} --casesdir ${CSEROOT} --res=120 --compiler=gnu -cp ${PRE} "
+CMD="_cheyenne_CBR.sh --srcroot ${SRCROOT} --casesdir ${CSEROOT} --res=120 --compiler=(gnu intel) -cp ${PRE} "
 
-#GDD# # Un-comment this to do a dry run
-#GDD# for C in ${COMPS[@]}; do
-#GDD# 	CCMD="${C}${CMD}"
-#GDD# 	LG_FILE="log.dryrun.${PRE}.${C}.txt"
-#GDD# 	./${CCMD} -dr 2>&1 | tee $LG_FILE
-#GDD# done
-#GDD# exit $?
+## # Un-comment this section to do a dry run
+## for C in ${COMPS[@]}; do
+## 	CCMD="${C}${CMD}"
+## 	LG_FILE="log.dryrun.${PRE}.${C}.txt"
+## 	./${CCMD} -dr 2>&1 | tee $LG_FILE
+## done
+## exit $?
 
 # Run the setup steps for each
 for C in ${COMPS[@]}; do
