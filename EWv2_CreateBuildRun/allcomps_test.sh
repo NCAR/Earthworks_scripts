03e9b6012d6626e232b84641e5ee052ccdbcc0f5
G. Dylan Dickerson <gdicker@ucar.edu>
2024-02-02 09:12:14 -0700

    Update allcomps_test.sh for Derecho and to add hints

--- EWv2_CreateBuildRun/allcomps_test.sh
+++ EWv2_CreateBuildRun/allcomps_test.sh
@@ -4,19 +4,27 @@
 # bash allcomps_test.sh 2>&1 | tee log.attXX.txt
 
 RESS=("120" "60" "30" "15")
+# RESS=("120")
 COMPS=("FHS94" "FKESSLER" "F2000climo" "QPC6" "FullyCoupled")
+# COMPS=("FullyCoupled")
 COMPI=("nvhpc" "intel-oneapi" "gnu")
+
 # Edit these paths to whatever works for you
 # Assumes script is in "EarthWorks/tools/Earthworks_scripts/EWv1_CreateBuildRun"
 SRCROOT="$(realpath ${PWD}/../../../)" # Points to EarthWorks
+if [ ! -d "${SRCROOT}" ] ; then
+	echo -e "ERROR: SRCROOT not a directory: \"${SRCROOT}\""
+	exit 1
+fi
 CSEROOT="$(realpath ${PWD}/../../../../cases)"
 [ -d "${CSEROOT}" ] || mkdir -p "${CSEROOT}"
 # Adjust case prefix to whatever makes sense for you
-PRE="$(date +%Y%b%d_%H%M%S)_EWMv2Testing"
-#INDATA="/glade/p/univ/ucsu0085/inputdata2"
+PRE="$(date +%Y%b%d_%H%M%S)_EWMTesting"
+INDATA="/glade/campaign/univ/ucsu0085/inputdata"
 
-#CMD="_derecho_CBR.sh -id ${INDATA} --srcroot ${SRCROOT} --casesdir ${CSEROOT} --res=(120 60 30) --compiler=(gnu intel) -cp ${PRE} "
-CMD="_derecho_CBR.sh --srcroot ${SRCROOT} --casesdir ${CSEROOT} --res=(${RESS[@]}) --compiler=(${COMPI[@]}) -cp ${PRE} "
+CMD="_derecho_CBR.sh -id ${INDATA} --srcroot ${SRCROOT} --casesdir ${CSEROOT} --res=(120 60 30) --compiler=(gnu intel) -cp ${PRE} "
+## Note the difference in RESS between above and below lines. Arrays don't work for single values correctly
+# CMD="_derecho_CBR.sh -id ${INDATA} --srcroot ${SRCROOT} --casesdir ${CSEROOT} --res=${RESS} --compiler=(${COMPI[@]}) -cp ${PRE} "
 
 ## # Un-comment this section to do a dry run
 ## for C in ${COMPS[@]}; do
